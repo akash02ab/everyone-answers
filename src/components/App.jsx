448db95ad6171942db6fd8e4c99fc34300a02d15
akash02ab <akash02.ab@gmail.com>
2021-07-13 15:57:34 +0530

    part4.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
 			dispatch(getStudents(user.email.replaceAll(".", "-")));
 			history.replace("/dashboard");
 		}
-	}, []);
+	}, [user]);
 
 	if (error || fetchError) {
 		return (
