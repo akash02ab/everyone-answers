33c31d6c865efb3f363da1d1e31af4e5db01cda7
akash02ab <akash02.ab@gmail.com>
2021-07-18 17:42:08 +0530

    link changed

--- src/components/SessionLink.jsx
+++ src/components/SessionLink.jsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 const SessionLink = ({ session }) => {
 	return (
 		<Typography variant="h5">
-			Student Link: <Link to={`/answer/${session}`}>{`http://localhost:3000/answer/${session}`}</Link>
+			Student Link:{" "}
+			<Link to={`/answer/${session}`}>{`https://everyone-answers.netlify.app/answer/${session}`}</Link>
 		</Typography>
 	);
 };
